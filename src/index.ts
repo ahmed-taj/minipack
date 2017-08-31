c82c73320478ac7e36c991d57d94cbb0da1f2750
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-08-31 13:50:35 +0300

    Export package as named module instead of default export

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import webpack = require('webpack')
 // Ours
 import { merge, CompilerOptions } from './config'
 
-const compiler = (options: CompilerOptions): webpack.Compiler => {
+const core = (options: CompilerOptions): webpack.Compiler => {
   return webpack(merge(options))
 }
 
-export default compiler
+export { core }
