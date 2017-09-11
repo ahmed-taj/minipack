c83f6127d55eae1ded1fab2dda32194f338f4fec
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-09-11 13:40:09 +0300

    feat(ES Next :fire:): Support ES6 and beyond using Babel

--- src/config/base.ts
+++ src/config/base.ts
@@ -18,7 +18,26 @@ const Base: Configuration = {
   module: {
     rules: [
       // CSS
-      { test: /\.css$/, loader: 'css-loader' }
+      {
+        test: /\.css$/,
+        loader: require.resolve('css-loader')
+      },
+      // Support ES6 (and beyond) using Babel
+      {
+        test: /\.(js|jsx|es)$/,
+        loader: require.resolve('babel-loader'),
+        exclude: /(node_modules|bower_components)/,
+        options: {
+          babelrc: false,
+          presets: [
+            require.resolve('babel-preset-env'),
+            require.resolve('babel-preset-react')
+          ],
+          // Enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true
+        }
+      }
     ]
   },
 
