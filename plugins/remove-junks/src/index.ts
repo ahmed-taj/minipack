febc4593e51c58d49b87b13981a37a5f0b95385e
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-09-21 17:36:35 +0300

    chore(remove-junks-plugin): tslint :fire:

--- plugins/remove-junks/src/index.ts
+++ plugins/remove-junks/src/index.ts
@@ -5,13 +5,13 @@ import { Plugin } from 'webpack'
  * A webpack plugin to remove junk chunk's files
  */
 class RemoveJunksPlugin implements Plugin {
-  constructor(private chunks: string[]) { }
+  constructor(private chunks: string[]) {}
 
-  apply(compiler) {
+  public apply(compiler) {
     compiler.plugin('emit', (compilation, callback) => {
-
       // filter chunks
-      compilation.chunks.filter(chunk => this.chunks.includes(chunk.name))
+      compilation.chunks
+        .filter(chunk => this.chunks.includes(chunk.name))
         .forEach(chunk => {
           // Remove chunk's files
           chunk.files.map(f => delete compilation.assets[f])
--- plugins/remove-junks/tests/integration.test.ts
+++ plugins/remove-junks/tests/integration.test.ts
@@ -4,17 +4,15 @@ import path = require('path')
 import { promisify } from 'util'
 
 // Packages
-import webpack = require('webpack')
 import test from 'ava'
+import webpack = require('webpack')
 
 const webpackAsync = promisify(webpack)
 
 const testCases = fs.readdirSync(path.join(__dirname, 'cases'))
 
 testCases.filter(tc => {
-
   test(tc, async t => {
-
     const testDir = path.join(__dirname, 'cases', tc)
     const expectDir = path.join(testDir, 'expected')
     const outDir = path.join(__dirname, 'actual', tc)
@@ -25,17 +23,16 @@ testCases.filter(tc => {
     config.output.path = outDir
 
     // Bundle with webpack
-    let stats = await webpackAsync(config)
-    if (stats['hasErrors']()) {
+    const stats = (await webpackAsync(config)) as webpack.Stats
+    if (stats.hasErrors()) {
       throw new Error(stats.toString())
     }
 
     // Assert output
     // We ignore .gitkeep files because we only add them to make sure they get
     // committed even if they are empty
-    const expected = fs.readdirSync(expectDir).filter(f => f != '.gitkeep')
+    const expected = fs.readdirSync(expectDir).filter(f => f !== '.gitkeep')
     const actual = fs.readdirSync(outDir)
     t.deepEqual(expected, actual)
   })
 })
-
